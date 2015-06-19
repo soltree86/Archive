7e043abe073ad77c2fd9b818b58f1f48cc7df11b
James Work <james.ha86@gmail.com>
2015-06-19 17:35:20 -0400

    Directive
    Added Directives

--- public/modules/archives/directives/archives.client.directive.js
+++ public/modules/archives/directives/archives.client.directive.js
@@ -1,8 +1,24 @@
 'use strict';
 
 // Archives Directive
-angular.module('archives').directive('', function() {
+angular.module('archives').directive('media', function() {
 	return {
-		template: 'N'
+		restrict: 'E',
+		link: function(scope, element, attrs) {
+			var selectedMedia,
+				media;
+
+			scope.$watch(attrs.info, function(value) {
+				scope.media = value;
+				if(value) {
+					scope.selectedMedia = value[0];
+				}
+			});
+
+			scope.setMedia = function(media) {
+				scope.selectedMedia = media;
+			};
+		},
+		templateUrl: 'modules/archives/views/media/directive-media.client.view.html'
 	};
 });
